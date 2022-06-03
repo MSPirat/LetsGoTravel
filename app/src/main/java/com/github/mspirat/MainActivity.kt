dd00d2d4e3aaf27b68e23420869a250ffcb40d31
Sergey Melnichenko <m1s3p@mail.ru>
2022-06-03 17:50:57 +0600

    Add scroll main screen (with test text)

--- app/src/main/java/com/github/mspirat/MainActivity.kt
+++ app/src/main/java/com/github/mspirat/MainActivity.kt
@@ -2,12 +2,23 @@ package com.github.mspirat
 
 //TODO if not needed delete adapter & models from main project directory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val secondScreenActivityBtn = findViewById<Button>(R.id.nextScreenBtn)
+        secondScreenActivityBtn.setOnClickListener(
+            View.OnClickListener {
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+            }
+        )
     }
 }
